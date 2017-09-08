d0c3ffbe540bb7e47c8afaafc62a298aadaf953c
gdmec07150942 <554061700@qq.com>
2017-09-08 18:07:00 +0800

    修改了首页

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -1,22 +1,57 @@
 <!DOCTYPE html>
 <html lang="en-gb">
-    <head>
-        <meta charset="UTF-8">
-        <title>@yield('title')</title>
-
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    </head>
-
-    @include('layouts.frontend.navbar')
-
-    <body>
-        <div class="container">
-            <div class="row">
-                @include('layouts.flash')
-                @yield('body')
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+<div id="container" data-PageSwitch>
+    <div class="sections">
+        <div class="section active" id="section0">
+            <div class="intro">
+                <h1 class="title">switchPage</h1>
+                <p>Create Beautiful Fullscreen Scrolling Websites</p>
             </div>
         </div>
-    </body>
+        <div class="section" id="section1">
+            <div class="intro">
+                <h1 class="title">Example</h1>
+                <p>HTML markup example to define 4 sections</p>
+                <img src="images/example.png"/>
+            </div>
+        </div>
+        <div class="section" id="section2">
+            <div class="intro">
+                <h1 class="title">Example</h1>
+                <p>The plug-in configuration parameters</p>
+                <img src="images/example2.png"/>
+            </div>
+        </div>
+        <div class="section" id="section3">
+            <div class="intro">
+                <h1 class="title">THE END</h1>
+                <p>Everything will be okay in the end. If it's not okay, it's not the end.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
 
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="pageswitch.js"></script>
+<script type="text/javascript">
+    $("#container").PageSwitch({
+        index: 0,    //页面开始的索引，0是第一页。
+        easing: "linear",   //动画效果。  ease
+        duration: 500,      //动画执行时间
+        loop: true,         //是否循环切换
+        pagination: true,   //是否进行分页
+        keybosrd: true,     //是否出发键盘事件
+        direction: "vertical",    //滑动方向：vertical（竖版）。horizontal（横板）
+        callback: function () {
+            $("h1").addClass("slide");
+        }
+    });
+</script>
 </html>
